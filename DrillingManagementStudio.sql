ff3441a5b1dec484bb7498f8c858d09c157e4f6d
gewazy <jurkowskidominik@gmail.com>
2024-03-15 16:31:34 +0100

    Add procedures and views
    Add some procedures and views

--- DrillingManagementStudio.sql
+++ DrillingManagementStudio.sql
@@ -1,22 +1,23 @@
 USE [u_dojurkow]
 GO
-/****** Object:  Table [dbo].[Attendance]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[InUse]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Attendance](
-	[driller_id] [int] NOT NULL,
-	[date] [date] NOT NULL,
-	[is_present] [bit] NOT NULL,
- CONSTRAINT [PK_Attendance] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[InUse](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[team_id] [int] NOT NULL,
+	[rig_id] [int] NOT NULL,
+	[date_from] [date] NOT NULL,
+	[date_to] [date] NULL,
+ CONSTRAINT [PK_InUse] PRIMARY KEY CLUSTERED 
 (
-	[driller_id] ASC,
-	[date] ASC
+	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Drillers]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[Drillers]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -33,13 +34,148 @@ CREATE TABLE [dbo].[Drillers](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  View [dbo].[VM_attendance_report]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[RigTypes]    Script Date: 15.03.2024 16:29:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[RigTypes](
+	[id] [tinyint] IDENTITY(1,1) NOT NULL,
+	[eq_type] [varchar](10) NOT NULL,
+ CONSTRAINT [PK_Table1] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [IX_Types] UNIQUE NONCLUSTERED 
+(
+	[eq_type] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Teams]    Script Date: 15.03.2024 16:29:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Teams](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[leader_id] [int] NOT NULL,
+	[start_date] [date] NOT NULL,
+	[end_date] [date] NULL,
+ CONSTRAINT [PK_Teans] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Rigs]    Script Date: 15.03.2024 16:29:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Rigs](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[type_id] [tinyint] NOT NULL,
+	[inventory_no] [varchar](20) NOT NULL,
+	[date_in] [date] NOT NULL,
+	[date_out] [date] NULL,
+ CONSTRAINT [PK_Devices] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  View [dbo].[VW_team_leaders]    Script Date: 15.03.2024 16:29:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create view [dbo].[VW_team_leaders]
+as
+select 
+	t.id as team_id
+	, d.first_name + ' ' + d.last_name as Brygadzista
+	, d.phone
+	, tp.eq_type + ' ' + r.inventory_no as Urzadzenie
+from Teams t 
+	join Drillers d on t.leader_id = d.id
+	join InUse i on d.team_id = i.team_id
+	join Rigs r on i.rig_id = r.id
+	join RigTypes tp on tp.id = r.type_id;
+GO
+/****** Object:  Table [dbo].[Planning]    Script Date: 15.03.2024 16:29:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Planning](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[station] [int] NOT NULL,
+	[team_id] [int] NOT NULL,
+	[add_date] [date] NOT NULL,
+	[expire_date] [date] NOT NULL,
+	[done] [date] NULL,
+	[canceled] [bit] NOT NULL,
+ CONSTRAINT [PK_Plan] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  View [dbo].[VW_Plan_punkty]    Script Date: 15.03.2024 16:29:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create view [dbo].[VW_Plan_punkty]
+as
+select p.station, tl.team_id, tl.Brygadzista, tl.Urzadzenie, expire_date 
+from Planning p join VW_Team_leaders tl on p.team_id = tl.team_id
+where canceled = 0 and done is null;
+GO
+/****** Object:  View [dbo].[VW_Plan_Brygady]    Script Date: 15.03.2024 16:29:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create view [dbo].[VW_Plan_Brygady]
+AS
+select 
+	team_id
+	, Brygadzista
+	, Urzadzenie
+	, isnull((select count(*) 
+			  from Planning 
+			  where 
+				team_id = tl.team_id 
+				and canceled = 0 
+				and done is null 
+			  group by team_id), 0) as Zaplanowano 
+from VW_team_leaders tl 
+GO
+/****** Object:  Table [dbo].[Attendance]    Script Date: 15.03.2024 16:29:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Attendance](
+	[driller_id] [int] NOT NULL,
+	[date] [date] NOT NULL,
+	[is_present] [bit] NOT NULL,
+ CONSTRAINT [PK_Attendance] PRIMARY KEY CLUSTERED 
+(
+	[driller_id] ASC,
+	[date] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  View [dbo].[VW_attendance_report]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE   VIEW [dbo].[VM_attendance_report]
+CREATE   VIEW [dbo].[VW_attendance_report]
 AS
 SELECT *
 FROM (
@@ -60,7 +196,7 @@ FROM (
 		FOR day IN ([1], [2], [3], [4], [5], [6], [7], [8] , [9], [10], [11], [12], [13], [14], [15], [16], [17], [18], [19], [20], [21], [22], [23], [24], [25], [26], [27], [28], [29], [30], [31])
 	) AS attendance_report;
 GO
-/****** Object:  Table [dbo].[Boreholes]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[Boreholes]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -89,13 +225,13 @@ CREATE TABLE [dbo].[Boreholes](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Failures]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[Failures]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Failures](
-	[id] [int] NOT NULL,
+	[id] [int] IDENTITY(1,1) NOT NULL,
 	[rig_id] [int] NOT NULL,
 	[start_date] [date] NOT NULL,
 	[fail_descrip] [varchar](256) NOT NULL,
@@ -108,7 +244,7 @@ CREATE TABLE [dbo].[Failures](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Fuel]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[Fuel]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -124,50 +260,14 @@ CREATE TABLE [dbo].[Fuel](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[InUse]    Script Date: 26.02.2024 18:22:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[InUse](
-	[id] [int] IDENTITY(1,1) NOT NULL,
-	[team_id] [int] NOT NULL,
-	[rig_id] [int] NOT NULL,
-	[date_from] [date] NOT NULL,
-	[date_to] [date] NULL,
- CONSTRAINT [PK_InUse] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Plan]    Script Date: 26.02.2024 18:22:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Plan](
-	[id] [int] IDENTITY(1,1) NOT NULL,
-	[station] [int] NOT NULL,
-	[team_id] [int] NOT NULL,
-	[add_date] [date] NOT NULL,
-	[expire_date] [date] NOT NULL,
-	[done] [date] NULL,
-	[canceled] [bit] NOT NULL,
- CONSTRAINT [PK_Plan] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[PositionDetails]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[PositionDetails]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[PositionDetails](
 	[id] [int] IDENTITY(1,1) NOT NULL,
-	[driler_id] [int] NOT NULL,
+	[driller_id] [int] NOT NULL,
 	[post_id] [tinyint] NOT NULL,
 	[date_from] [date] NOT NULL,
 	[date_to] [date] NULL,
@@ -177,7 +277,7 @@ CREATE TABLE [dbo].[PositionDetails](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Positions]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[Positions]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -195,7 +295,7 @@ CREATE TABLE [dbo].[Positions](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Removal]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[Removal]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -211,40 +311,7 @@ CREATE TABLE [dbo].[Removal](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Rigs]    Script Date: 26.02.2024 18:22:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Rigs](
-	[id] [int] IDENTITY(1,1) NOT NULL,
-	[type_id] [tinyint] NOT NULL,
-	[inventory_no] [varchar](20) NOT NULL,
-	[date_in] [date] NOT NULL,
-	[date_out] [date] NULL,
- CONSTRAINT [PK_Devices] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Teams]    Script Date: 26.02.2024 18:22:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Teams](
-	[id] [int] IDENTITY(1,1) NOT NULL,
-	[leader_id] [int] NOT NULL,
-	[start_date] [date] NOT NULL,
-	[end_date] [date] NULL,
- CONSTRAINT [PK_Teans] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Theory]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[Theory]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -262,25 +329,7 @@ CREATE TABLE [dbo].[Theory](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Types]    Script Date: 26.02.2024 18:22:34 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Types](
-	[id] [tinyint] IDENTITY(1,1) NOT NULL,
-	[eq_type] [varchar](10) NOT NULL,
- CONSTRAINT [PK_Table1] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
- CONSTRAINT [IX_Types] UNIQUE NONCLUSTERED 
-(
-	[eq_type] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[WorkHours]    Script Date: 26.02.2024 18:22:34 ******/
+/****** Object:  Table [dbo].[WorkHours]    Script Date: 15.03.2024 16:29:45 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -310,11 +359,11 @@ ALTER TABLE [dbo].[Fuel] ADD  CONSTRAINT [DF_Fuel_date]  DEFAULT (getdate()) FOR
 GO
 ALTER TABLE [dbo].[InUse] ADD  CONSTRAINT [DF_InUse_date_from]  DEFAULT (CONVERT([date],getdate())) FOR [date_from]
 GO
-ALTER TABLE [dbo].[Plan] ADD  CONSTRAINT [DF_Plan_add_date]  DEFAULT (CONVERT([date],getdate())) FOR [add_date]
+ALTER TABLE [dbo].[Planning] ADD  CONSTRAINT [DF_Plan_add_date]  DEFAULT (CONVERT([date],getdate())) FOR [add_date]
 GO
-ALTER TABLE [dbo].[Plan] ADD  CONSTRAINT [DF_Plan_expire_date]  DEFAULT (CONVERT([date],dateadd(day,(14),getdate()))) FOR [expire_date]
+ALTER TABLE [dbo].[Planning] ADD  CONSTRAINT [DF_Plan_expire_date]  DEFAULT (CONVERT([date],dateadd(day,(14),getdate()))) FOR [expire_date]
 GO
-ALTER TABLE [dbo].[Plan] ADD  CONSTRAINT [DF_Plan_canceled]  DEFAULT ((0)) FOR [canceled]
+ALTER TABLE [dbo].[Planning] ADD  CONSTRAINT [DF_Plan_canceled]  DEFAULT ((0)) FOR [canceled]
 GO
 ALTER TABLE [dbo].[PositionDetails] ADD  CONSTRAINT [DF_PositionDetails_date_from]  DEFAULT (CONVERT([date],getdate())) FOR [date_from]
 GO
@@ -366,17 +415,17 @@ REFERENCES [dbo].[Teams] ([id])
 GO
 ALTER TABLE [dbo].[InUse] CHECK CONSTRAINT [FK_InUse_Teams]
 GO
-ALTER TABLE [dbo].[Plan]  WITH CHECK ADD  CONSTRAINT [FK_Plan_Teams] FOREIGN KEY([team_id])
+ALTER TABLE [dbo].[Planning]  WITH CHECK ADD  CONSTRAINT [FK_Plan_Teams] FOREIGN KEY([team_id])
 REFERENCES [dbo].[Teams] ([id])
 GO
-ALTER TABLE [dbo].[Plan] CHECK CONSTRAINT [FK_Plan_Teams]
+ALTER TABLE [dbo].[Planning] CHECK CONSTRAINT [FK_Plan_Teams]
 GO
-ALTER TABLE [dbo].[Plan]  WITH CHECK ADD  CONSTRAINT [FK_Plan_Theory] FOREIGN KEY([station])
+ALTER TABLE [dbo].[Planning]  WITH CHECK ADD  CONSTRAINT [FK_Plan_Theory] FOREIGN KEY([station])
 REFERENCES [dbo].[Theory] ([Station])
 GO
-ALTER TABLE [dbo].[Plan] CHECK CONSTRAINT [FK_Plan_Theory]
+ALTER TABLE [dbo].[Planning] CHECK CONSTRAINT [FK_Plan_Theory]
 GO
-ALTER TABLE [dbo].[PositionDetails]  WITH CHECK ADD  CONSTRAINT [FK_PositionDetails_Drillers] FOREIGN KEY([driler_id])
+ALTER TABLE [dbo].[PositionDetails]  WITH CHECK ADD  CONSTRAINT [FK_PositionDetails_Drillers] FOREIGN KEY([driller_id])
 REFERENCES [dbo].[Drillers] ([id])
 GO
 ALTER TABLE [dbo].[PositionDetails] CHECK CONSTRAINT [FK_PositionDetails_Drillers]
@@ -386,8 +435,9 @@ REFERENCES [dbo].[Positions] ([id])
 GO
 ALTER TABLE [dbo].[PositionDetails] CHECK CONSTRAINT [FK_PositionDetails_Positions]
 GO
-ALTER TABLE [dbo].[Removal]  WITH CHECK ADD  CONSTRAINT [FK_Removal_Boreholes] FOREIGN KEY([BH_id])
+ALTER TABLE [dbo].[Removal]  WITH NOCHECK ADD  CONSTRAINT [FK_Removal_Boreholes] FOREIGN KEY([BH_id])
 REFERENCES [dbo].[Boreholes] ([id])
+NOT FOR REPLICATION 
 GO
 ALTER TABLE [dbo].[Removal] CHECK CONSTRAINT [FK_Removal_Boreholes]
 GO
@@ -397,7 +447,7 @@ GO
 ALTER TABLE [dbo].[Removal] CHECK CONSTRAINT [FK_Removal_Teams]
 GO
 ALTER TABLE [dbo].[Rigs]  WITH CHECK ADD  CONSTRAINT [FK_Rigs_Types] FOREIGN KEY([type_id])
-REFERENCES [dbo].[Types] ([id])
+REFERENCES [dbo].[RigTypes] ([id])
 GO
 ALTER TABLE [dbo].[Rigs] CHECK CONSTRAINT [FK_Rigs_Types]
 GO
@@ -441,6 +491,87 @@ GO
 ALTER TABLE [dbo].[WorkHours]  WITH CHECK ADD  CONSTRAINT [CK_WorkHours] CHECK  (([hours]=(12) OR [hours]=(11) OR [hours]=(10) OR [hours]=(9) OR [hours]=(8)))
 GO
 ALTER TABLE [dbo].[WorkHours] CHECK CONSTRAINT [CK_WorkHours]
+GO
+/****** Object:  StoredProcedure [dbo].[p_plan_brygady]    Script Date: 15.03.2024 16:29:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[p_plan_brygady]
+@name varchar(50)
+as
+begin
+	-- sprawdza czy istnieje brygadzista o podanym imieniu i nazwisku, korzysta z widoku VW_team_leaders
+	declare @msg1 varchar(50) = @name + ' nie istnieje lub nie ma brygady!';
+	if not exists (select * from VW_team_leaders where Brygadzista = @name)
+		throw 50001, @msg1, 1;
+
+	select * from Planning 
+	where team_id = (select team_id 
+					 from VW_team_leaders 
+					 where Brygadzista = @name)
+		  and done is null 
+		  and canceled = 0;
+end;
+GO
+/****** Object:  StoredProcedure [dbo].[p_team_plan]    Script Date: 15.03.2024 16:29:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+create   proc [dbo].[p_team_plan]
+@name varchar(50), --imie i nazwisko brygadzisty
+@start int, --początek przedziału do przeszukiwania tabeli theory 
+@end int  --koniec przedziału do przeszukiwania tabeli theory
+as
+begin
+	-- sprawdza czy istnieje brygadzista o podanym imieniu i nazwisku, korzysta z widoku VW_team_leaders
+	declare @msg1 varchar(50) = @name + ' nie istnieje lub nie ma brygady!';
+	if not exists (select * from VW_team_leaders where Brygadzista = @name)
+		throw 50001, @msg1, 1;
+	
+	-- sprawdza czy w zadeklarowanym przedziale istnieją jakieś punkty do wiercenia w tabeli Theory
+	if not exists (select * 
+				   from Theory 
+				   where drilling_date is null 
+				         and station between @start and @end)
+		throw 50002, 'W podanym zakresie nie ma punktów do wiercenia', 1;
+
+	-- zmienna potrzebna do wyszukania właściwych punktów w tabeli Theory, na etapie tyczenia jest deklarowana tylko metoda wiercenia
+	-- (ręcznie 'xr' lub traktor 'xt'), jedynie 'PAT' jest urządzeniem zamontowanym na traktorze, inne urządzenia to metoda ręczna
+	-- urządzenie jest przypisane do danego wiertacza
+	declare @ur varchar(2) 
+	set @ur = (select case
+						when Urzadzenie like 'PAT%' then 'xt'
+						else 'xr'
+						end
+				from VW_team_leaders where Brygadzista = @name);
+	
+	-- anulowanie zaplanowanych punktów jeśli nie zostały jeszcze wywiercone.
+	update planning
+	set canceled = 1
+	where station in 
+		(select station from theory 
+		 where station between @start and @end 
+			   and Descriptor = @ur 
+			   and drilling_date is null) 
+		and done is null;
+	
+	-- wstawienie nowych rekordów (plan)
+	insert planning (station, team_id)
+	select 
+		station,
+		(select team_id from VW_team_leaders where Brygadzista = @name)
+	from 
+		theory
+	where
+		station between @start and @end 
+		and Descriptor = @ur
+		and drilling_date is null;
+end;
 GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'FK_drillers' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Attendance', @level2type=N'COLUMN',@level2name=N'driller_id'
 GO
